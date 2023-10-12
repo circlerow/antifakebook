03a324697f58b1fd59a4a61419fc718149537a3a
unknown <tranbachchy2002@gmail.com>
2023-10-12 16:07:12 +0700

    feat: add login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/presentation/home.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
+import 'package:flutter_application/presentation/login.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
 
 void main() async {
-  await dot_env.dotenv.load();
+  // await dot_env.dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -13,8 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return MaterialApp(
+      home: Login(),
     );
   }
 }
