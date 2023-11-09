b1779a5318d88ba535120cbd31d2e00065996c67
admin <admin@admins-MacBook-Air.local>
2023-11-09 16:19:20 +0700

    9/11

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application/models/user_notification.dart';
+
+class FirebaseAPI {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fCMToken = await _firebaseMessaging.getToken();
+
+    FirebaseMessaging.onMessage.listen((message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.notification!.title.toString()}');
+      notifications.insert(
+          0,
+          new UserNotification(
+              imageUrl: 'assets/img/fb.jpg',
+              content: message.notification!.title.toString(),
+              time: "now"));
+    });
+    print('token $fCMToken');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application/api/firebase_api.dart';
+import 'package:flutter_application/firebase_options.dart';
+import 'package:flutter_application/presentation/home/home.dart';
 
 import 'package:flutter_application/presentation/login/login.dart';
 import 'package:flutter_application/presentation/signup/signUp.dart';
@@ -7,11 +11,15 @@ import 'package:flutter_application/presentation/signup/signUp.dart';
 
 void main() async {
   //await dot_env.dotenv.load();
-  runApp(const MyApp());
+  await WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAPI().initNotifications();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // MyApp({super.key});
 
   // This widget is the root of your application.
   @override
